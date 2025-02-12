aa353de1da495afd12e6cd233cc51778b33baaa1
Kim Doo Hyeon <engus525@naver.com>
2025-02-13 00:57:56 +0900

    [FEATURE] JPA 구현 #1
    - crypto
    - converter

--- src/main/java/young/blaybus/config/SwaggerConfig.java
+++ src/main/java/young/blaybus/config/SwaggerConfig.java
@@ -2,12 +2,8 @@ package young.blaybus.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
