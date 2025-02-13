a7c61c5ec603027df60d83ed66ce2be1dd255ff8
Kim Doo Hyeon <engus525@naver.com>
2025-02-13 17:51:05 +0900

    [FEATURE] ENUM API #6
    - 요일 목록
    - 보조 목록

--- src/main/java/young/blaybus/util/enums/assist/FoodAssist.java
+++ src/main/java/young/blaybus/util/enums/assist/FoodAssist.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum FoodAssist {
 
-  GRADE1("경관식 보조"),
-  GRADE2("죽, 반찬 등 요리 필요"),
+  GRADE4("스스로 식사 가능"),
   GRADE3("식사 차려드리기"),
-  GRADE4("스스로 식사 가능");
+  GRADE2("죽, 반찬 등 요리 필요"),
+  GRADE1("경관식 보조");
 
   private final String value;
 }
