d07e3ddc7f0d019c65b31de41c6a4fbf6a72c0d2
JeongIna <jeongina89@gmail.com>
2025-02-15 00:37:54 +0900

    refactor: JobSearchAreaRequest 필드명 수정

--- src/main/java/young/blaybus/domain/job_search/request/JobSearchAreaRequest.java
+++ src/main/java/young/blaybus/domain/job_search/request/JobSearchAreaRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "근무 가능 지역 입력 객체")
 public record JobSearchAreaRequest(
-        String cityDo,
+        String city,
         String guGun,
         String dong
 ) {
