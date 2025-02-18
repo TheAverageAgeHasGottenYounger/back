dee7c915d32f8da60d71cbc2318d77ce9e57be34
Kim Doo Hyeon <engus525@naver.com>
2025-02-19 05:59:26 +0900

    [ALL] 이것저것 #43
    - 어르신 상세 조회 시급 추가
    - 어르신 상세 조회 enum value 목록 추가

--- src/main/java/young/blaybus/domain/senior/service/DetailSeniorService.java
+++ src/main/java/young/blaybus/domain/senior/service/DetailSeniorService.java
@@ -1,10 +1,16 @@
 package young.blaybus.domain.senior.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import young.blaybus.domain.senior.controller.response.DetailSeniorResponse;
 import young.blaybus.domain.senior.repository.DetailSeniorRepository;
+import young.blaybus.util.enums.DayOfWeek;
+import young.blaybus.util.enums.assist.FoodAssist;
+import young.blaybus.util.enums.assist.LifeAssist;
+import young.blaybus.util.enums.assist.MoveAssist;
+import young.blaybus.util.enums.assist.ToiletAssist;
 
 @Service
 @RequiredArgsConstructor
@@ -16,11 +22,22 @@ public class DetailSeniorService {
 
   public DetailSeniorResponse getSenior(Long seniorId) {
     DetailSeniorResponse senior = detailSeniorRepository.getSenior(seniorId);
-    senior.setDayList(detailSeniorRepository.getSeniorDayList(seniorId));
-    senior.setLifeAssistList(detailSeniorRepository.getLifeAssistList(seniorId));
-    senior.setFoodAssistList(detailSeniorRepository.getFoodAssistList(seniorId));
-    senior.setMoveAssistList(detailSeniorRepository.getMoveAssistList(seniorId));
-    senior.setToiletAssistList(detailSeniorRepository.getToiletAssistList(seniorId));
+    List<DayOfWeek> seniorDayList = detailSeniorRepository.getSeniorDayList(seniorId);
+    List<LifeAssist> lifeAssistList = detailSeniorRepository.getLifeAssistList(seniorId);
+    List<FoodAssist> foodAssistList = detailSeniorRepository.getFoodAssistList(seniorId);
+    List<MoveAssist> moveAssistList = detailSeniorRepository.getMoveAssistList(seniorId);
+    List<ToiletAssist> toiletAssistList = detailSeniorRepository.getToiletAssistList(seniorId);
+
+    senior.setDayList(seniorDayList);
+    senior.setLifeAssistList(lifeAssistList);
+    senior.setFoodAssistList(foodAssistList);
+    senior.setMoveAssistList(moveAssistList);
+    senior.setToiletAssistList(toiletAssistList);
+    senior.setDayValueList(seniorDayList.stream().map(DayOfWeek::toString).toList());
+    senior.setLifeAssistValueList(lifeAssistList.stream().map(LifeAssist::getValue).toList());
+    senior.setFoodAssistValueList(foodAssistList.stream().map(FoodAssist::getValue).toList());
+    senior.setMoveAssistValueList(moveAssistList.stream().map(MoveAssist::getValue).toList());
+    senior.setToiletAssistValueList(toiletAssistList.stream().map(ToiletAssist::getValue).toList());
 
     return senior;
   }
