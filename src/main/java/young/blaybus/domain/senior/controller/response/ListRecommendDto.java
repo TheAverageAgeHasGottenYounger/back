f31a0f0002edafafc37ef56f8b223eeddd940151
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-02-19 01:05:34 +0900

    Revert "[TASK] JobSearch 도메인 구조 변경으로 인한 로직 수정"

--- src/main/java/young/blaybus/domain/senior/controller/response/ListRecommendDto.java
+++ src/main/java/young/blaybus/domain/senior/controller/response/ListRecommendDto.java
@@ -8,7 +8,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import young.blaybus.domain.job_search.response.JobSearchTimeSlotResponse;
 import young.blaybus.util.enums.DayOfWeek;
 
 @Builder
@@ -27,8 +26,15 @@ public class ListRecommendDto {
   @Schema(description = "프로필 사진")
   private String profileUrl;
 
-  @Schema(description = "근무 가능 요일&시간대 목록")
-  private List<TimeSlotDto> timeSlots;
+  @Setter
+  @Schema(description = "희망 요일 목록")
+  private List<DayOfWeek> dayList;
+
+  @Schema(description = "희망 시작 시간")
+  private LocalTime startTime;
+
+  @Schema(description = "희망 종료 시간")
+  private LocalTime endTime;
 
   @Schema(description = "돌봄 스타일")
   private String careStyle;
